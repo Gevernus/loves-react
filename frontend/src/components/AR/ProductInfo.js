6c2d99d78ab013c17691d499a55f16c418d4eeea
Artem Povaliaev <gevernus@mail.ru>
2025-01-06 15:40:03 +0400

    Added popup component

--- frontend/src/components/AR/ProductInfo.js
+++ frontend/src/components/AR/ProductInfo.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useCart } from '../../context/CartContext';
+import ProductInfoPopup from "./ProductInfoPopup";
 
 const ProductInfo = ({ product }) => {
     const { addToCart } = useCart();
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const openPopup = () => {
+        setIsPopupOpen(true);
+    };
+
+    const closePopup = () => {
+        console.log("Trying to close");
+        setIsPopupOpen(false);
+    };
 
     const handleAddToCart = (item) => {
         addToCart(item);
@@ -13,9 +24,10 @@ const ProductInfo = ({ product }) => {
                 className="product-image"
                 src={product.image}
                 alt={product.name}
+                onClick={openPopup}
             />
-            <div className="product-details">
-                <div className="product-title">{product.name}</div>
+            <div className="product-details" >
+                <div className="product-title" onClick={openPopup}>{product.name}</div>
                 <img
                     className="recipe-button"
                     src="/buy.png"
@@ -23,6 +35,11 @@ const ProductInfo = ({ product }) => {
                     onClick={() => handleAddToCart(product)}
                 />
             </div>
+            <ProductInfoPopup
+                isOpen={isPopupOpen}
+                onClose={closePopup}
+                product={product}
+            />
         </div>
     );
 };
