7e68ae833c1aa397f1f30a1bb3cb7416e45cb7dc
ItalianecXXX <anatoly.bezrukoff@ya.ru>
2025-01-08 18:25:34 +0700

    Popap stylize

--- frontend/src/components/AR/ProductInfoPopup.js
+++ frontend/src/components/AR/ProductInfoPopup.js
@@ -15,26 +15,27 @@ const ProductInfoPopup = ({ isOpen, onClose, product }) => {
         <Modal
             isOpen={isOpen}
             onRequestClose={onClose}
-            className="fixed inset-0 flex items-center justify-center z-50"
+            className="fixed inset-0 flex items-center justify-center z-50 px-4 py-16"
             overlayClassName="fixed inset-0 bg-black bg-opacity-50 z-40"
         >
-            <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-lg relative">
+            <div className="bg-white rounded-lg shadow-lg p-4 w-full max-w-lg relative">
                 <button
                     onClick={handleCloseClick}
-                    className="absolute top-4 right-4 text-gray-600 hover:text-gray-900 text-xl"
+                    className="absolute top-4 right-4 w-6 h-6 bg-neutral-100 rounded-lg text-gray-600 hover:text-gray-900 text-xl"
                 >
-                    ✖
+                     <div class="absolute w-3 h-0.5 bg-neutral-300 rotate-45 top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"></div>
+                     <div class="absolute w-3 h-0.5 bg-neutral-300 -rotate-45 top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"></div>
                 </button>
-                <h2 className="text-xl font-bold mb-4">{product.name}</h2>
-                <p className="text-gray-700 mb-2">{product.description}</p>
-                <p className="text-gray-800 font-semibold mb-4">
-                    <span className="text-gray-500">Price:</span> ${product.price}
-                </p>
                 <img
                     src={product.image}
                     alt={product.name}
-                    className="w-full rounded-md"
+                    className="max-h-96 rounded-md mb-8"
                 />
+                <div className="flex flex-wrap">
+                    <h2 className="w-full max-w-64 text-xl font-medium mb-1">{product.name}</h2>
+                    <p className="text-gray-800 text-xl font-semibold pl-8">{product.price}₽</p>
+                    <p className="w-full font-normal text-base text-gray-700 mb-2">{product.description}</p>
+                </div>
             </div>
         </Modal>
     );
