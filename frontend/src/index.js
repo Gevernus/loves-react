04f94849215b4584f39d033011a0a120ae207fb7
Artem Povaliaev <gevernus@mail.ru>
2025-01-21 02:37:13 +0400

    Disable eruda

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/npm/eruda';
-script.onload = () => {
-  window.eruda.init();
-};
-document.body.appendChild(script);
+// const script = document.createElement('script');
+// script.src = 'https://cdn.jsdelivr.net/npm/eruda';
+// script.onload = () => {
+//   window.eruda.init();
+// };
+// document.body.appendChild(script);
 root.render(
   // <React.StrictMode>
     <App />
